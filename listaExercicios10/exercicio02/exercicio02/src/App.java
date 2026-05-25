b593e4429fa48cbcf5f4b7dc3c09496dd9c891cc
texmalado <rafaelcaetano0931@gmail.com>
2026-05-25 10:17:32 -0300

    removendo pasta errada

--- listaExercicios10/exercicio02/exercicio02/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    // Nome: Rafael Caetano Braga
-    // Matrícula: 1261937255
-    public static void main(String[] args) throws Exception {
-        Aluno aluno1 = new Aluno("Rafael", "1261", 10, 8);
-
-        aluno1.exibirBoletim();
-    }
-}
--- listaExercicios10/exercicio06/exercicio06/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    // Nome: Rafael Caetano Braga
-    // Matrícula: 1261937255
-    public static void main(String[] args) throws Exception {
-        Filme filme = new Filme("Interestelar", "Christopher Nolan", 2014, 169, 9.2);
-
-        filme.exibitDetalhes();
-    }
-}
